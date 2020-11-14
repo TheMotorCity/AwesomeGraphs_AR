5557a2847e982676b59d6333201a389872a85090
TheMotorCity <leo_zgr@outlook.com>
2020-11-14 18:27:00 +0100

    check boxes are added on the click event

--- Assets/Scripts/DataSelectionMenu.cs
+++ Assets/Scripts/DataSelectionMenu.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,6 +9,8 @@ public class DataSelectionMenu : MonoBehaviour
 
     public Button InvokeDataSelectionMenu;
     public Toggle ToggleUI;
+
+    String[] criteria = {"Deaths", "Cases", "Recoveries", "Germany", "Italy", "US"};
     // Start is called before the first frame update
     void Start()
     {
@@ -16,16 +19,24 @@ public class DataSelectionMenu : MonoBehaviour
     }
 
     void TaskOnClick(){
-		Toggle checkBox = Instantiate(ToggleUI, new Vector3(0,0,0), Quaternion.identity);
-        checkBox.transform.SetParent(gameObject.transform);
-        checkBox.transform.localScale = new Vector3(1,1,1);
-        RectTransform recTransform = checkBox.transform.GetComponent(typeof(RectTransform)) as RectTransform;
-        recTransform.anchoredPosition = new Vector3(0,0,0);
-        Debug.Log(recTransform.anchoredPosition);
-        recTransform.anchorMax = new Vector2(0,0);
-        recTransform.anchorMin = new Vector2(0,0);
-        recTransform.position = new Vector3(50,100,0);
-      
+
+        for (int i = 0; i < criteria.Length; i++)
+        {
+            Toggle checkBox = Instantiate(ToggleUI, new Vector3(0,0,0), Quaternion.identity);
+            RectTransform recTransform = checkBox.transform.GetComponent<RectTransform>();
+            checkBox.transform.SetParent(gameObject.transform);
+
+            float leftBorderPosX = -(gameObject.transform.GetComponent<RectTransform>().sizeDelta.x/2f);
+            float bottomBorderPosY = -(gameObject.transform.GetComponent<RectTransform>().sizeDelta.y/2f);
+
+            recTransform.pivot = new Vector2(0,0);
+            checkBox.transform.localScale = new Vector3(1,1,1);
+            recTransform.localPosition = new Vector3(leftBorderPosX+10,bottomBorderPosY+(80 + i*20),0);
+            recTransform.rotation = gameObject.transform.rotation;
+            checkBox.transform.GetChild(1).GetComponent<Text>().text = criteria[i];
+
+        }
+
 	}
 
     // Update is called once per frame
